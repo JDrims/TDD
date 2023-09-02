053439c795f0a2e5fb2d3dcbcea76d2005cc526a
JDrims <dimas.asus@mail.ru>
2023-09-03 04:31:25 +0600

    findByNumber implemented

--- src/main/java/prog/PhoneBook.java
+++ src/main/java/prog/PhoneBook.java
@@ -1,16 +1,24 @@
 package prog;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class PhoneBook {
     Map<String, String> list = new TreeMap<>();
     public int add(String name, String number) {
-        list.put(name, number);
+        if(!list.containsKey(name)) {
+            list.put(name, number);
+        }
         return list.size();
     }
 
     public String findByNumber(String number) {
+        for(String key: list.keySet()) {
+            if(number.equals(list.get(key))) {
+                return key;
+            }
+        }
         return null;
     }
 }
